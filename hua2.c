e54eceda862ea34fcf7067d0c67d421235016cdd
pas4ik <pas4ik@gmail.com>
2021-06-27 13:37:03 +0300

    Update hua2.c

--- hua2.c
+++ hua2.c
@@ -3,39 +3,89 @@
 
 int main()
 {
-    int i, j, Ncom, Nout, debug = 1;
-    char com_list[2000][20];
-    char out_list[2000][16];
+    int i, j, Ncom, debug = 0;
+    int com_done;
+    char com_list[2000][21];
+
+    struct {
+        char mac_now[13];
+        char mac_was[13];
+    } ip_pool[256];
 
     // please define the C input here. FOR EXAMPLE: int n; scanf("%d",&n);
 
     scanf("%d", &Ncom);
     for (i = 1; i <= Ncom; i++)
     {
-        scanf("%s", &com_list[i][0]);
+        //printf("%d?", i);
+        scanf("%s", &com_list[i]);
     }
 
+    for (i = 1; i < 255; i++) { strcpy(ip_pool[i].mac_now, ""); strcpy(ip_pool[i].mac_was, ""); }
+    //for (i = 1; i <= Ncom; i++) printf("%d : %s\n", i, com_list[i]);
     // please finish the FUNCTION body here.
 
+    //strcpy(ip_pool[1].mac_was, "aaaabbbbcccc");
     if (debug)  printf("%s \n", "\nstart");
+    com_done = 0;
 
     for (i = 1; i <= Ncom; i++)
     {
-        if (debug) printf("%s:%d", "n: ", i);
-        if (!strncmp((const char*)com_list[i],"REQUEST",8))
+        if (debug) printf("%s%d ", "i: ", i);
+        com_done = 0;
+        if (!strncmp((const char*)com_list[i],"REQUEST",7))
         {
-            if (debug) printf("%s \n", "req");
+            if (debug) printf("%s%s ", "req: ", com_list[i]);
+            for (j = 0; j < 255; j++)       // search if it was before
+            {
+                if ((strcmp((ip_pool[j].mac_was), "") != 0) &&
+                (strstr((const char*)com_list[i], (const char*)ip_pool[j].mac_was) != NULL))
+                {
+                    if (debug) printf("%s ", ip_pool[j].mac_was);
+                    if (debug) printf("%s%d ", "was in ", j);
+                    strcpy((char *)ip_pool[j].mac_now, (const char *)&com_list[i][8]);  // allocate again
+                    if (debug) printf("%s", "allocated again");
+                    printf("%s%d\n", "192.168.0.", j);
+                    com_done = 1;
+                    break;
+                }
+            }
+            if (!com_done)  // if it not was before
+            {
+                for (j = 0; j < 255; j++)
+                {
+                    if (strcmp((ip_pool[j].mac_was), "") == 0)      // search first empty
+                    {
+                        strcpy((char *)ip_pool[j].mac_now, (const char *)&com_list[i][8]);
+                        strcpy((char *)ip_pool[j].mac_was, (const char *)ip_pool[j].mac_now);
+                        if (debug) printf("%s", "allocated new");
+                        printf("%s%d\n", "192.168.0.", j);
+                        com_done = 1;
+                        break;
+                    }
+                }
+            }
+            if (!com_done) printf("%s\n", "NA");
         } else
+        if (!strncmp((const char*)com_list[i],"RELEASE",7))
         {
-            if (debug) printf("%s \n", "rel");
+            if (debug) printf("%s%s ", "req: ", com_list[i]);
+            for (j = 0; j < 255; j++)       // search if allocated
+            {
+                if ((strcmp((ip_pool[j].mac_now), "") != 0) &&
+                (strstr((const char*)com_list[i], (const char*)ip_pool[j].mac_now) != NULL))
+                {
+                    if (debug) printf("%s ", ip_pool[j].mac_now);
+                    if (debug) printf("%s %d ", "allocated in", j);
+                    if (debug) printf("%s%d\n", "released 192.168.0.", j);
+                    com_done = 1;
+                    break;
+                }
+            }
         }
     }
-    Nout = 0;
+    
     // please define the C output here. FOR EXAMPLE: printf("%d",a);
-
-    printf("%s \n", "output:");
-    for (i = 1; i <= Nout; i++)
-        printf("%s \n", out_list[i]);
 
     return 0;
 }
